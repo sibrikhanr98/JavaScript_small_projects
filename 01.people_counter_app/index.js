c212f30a46da14876e56f5412f834a3bf3e4058d
sibrikhanr98 <rmsifrikan6500@gmail.com>
2024-05-17 16:41:12 +0530

    increment has been added while clicking

--- 01.people_counter_app/index.js
+++ 01.people_counter_app/index.js
@@ -1,1 +1,8 @@
 // document.getElementById('count-el').innerText=5
+let count = 0
+let countEL = document.getElementById('count-el')
+function increment() 
+{
+    count = count + 1
+    countEL.innerText = count
+}
