bb260929785d51f875426ec89c109a31ea2224b5
sibrikhanr98 <rmsifrikan6500@gmail.com>
2024-05-19 16:27:55 +0530

    set it to display the sum

--- 02.Black_jack_game/index.js
+++ 02.Black_jack_game/index.js
@@ -6,23 +6,26 @@ let sum = firstCard + secondCard
 let hasBlackjack = false
 let isAlive = true
 let message = ''
-
+let messageEl = document.getElementById('message-el')
+let sumEl = document.getElementById('sum-el')
 function startgame()
 {
+    sumEl.textContent = "Sum: " + sum
+        
     if (sum <= 21)
-{
-    message = "Do you want to draw a new card? 🙂"
-}
-else if (sum === 21)
-{
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackjack = true
-}
-else
-{
-    message = "You're out of the game! 😭"
-    isAlive = false
-}
-
-    console.log("isAlive")
+        {
+            message = "Do you want to draw a new card? "
+        }
+    else if (sum === 21)
+        {
+            message = "Wohoo! You've got Blackjack! "
+            hasBlackjack = true
+        }
+    else
+        {
+            message = "You're out of the game! "
+            isAlive = false
+        }
+    messageEl.textContent = message
+    
 }
