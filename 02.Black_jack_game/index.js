96d7a8a0ebfbe7de972478300e591a88793dc38a
sibrikhanr98 <rmsifrikan6500@gmail.com>
2024-05-19 18:18:21 +0530

    function that randomly generate the card was added

--- 02.Black_jack_game/index.js
+++ 02.Black_jack_game/index.js
@@ -1,8 +1,8 @@
-let firstCard = 4
-let secondCard = 11
+let firstCard = randomCard()
+let secondCard = randomCard()
 
 let sum = firstCard + secondCard
-
+let cards = [firstCard, secondCard]
 let hasBlackjack = false
 let isAlive = true
 let message = ''
@@ -10,13 +10,36 @@ let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardEl = document.getElementById('card-el')
 
+
+function randomCard()
+{
+    let randomCards = Math.floor(Math.random()*13)+1
+    if (randomCards > 10)
+    {
+        return randomCards = 10
+    }
+    else if (randomCards === 1)
+    {
+        return randomCards = 11
+    }
+    else
+    {
+        return randomCards
+    }
+}
+
 function startGame()
 {
     renderGame()
 }
 function renderGame()
 {
-    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardEl.textContent = "Cards: "
+
+    for (let i = 0; i<cards.length; i++)
+    {
+        cardEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
         
     if (sum <= 20)
@@ -39,7 +62,8 @@ function renderGame()
 function newCard()
 {
     // console.log("Drawing new card from the deck!")
-    let card = 4
+    let card = randomCard()
     sum += card
+    cards.push(card)
     renderGame()
 }
