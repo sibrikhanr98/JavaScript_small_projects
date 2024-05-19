37f4f9a689f5a90a6b8b9f635d030dd5f649f60c
sibrikhanr98 <rmsifrikan6500@gmail.com>
2024-05-19 16:30:07 +0530

    set it to display the cards

--- 02.Black_jack_game/index.js
+++ 02.Black_jack_game/index.js
@@ -8,8 +8,10 @@ let isAlive = true
 let message = ''
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
+let cardEl = document.getElementById('card-el')
 function startgame()
 {
+    cardEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: " + sum
         
     if (sum <= 21)
