d5fc2fcc7197038b56fcf78883a08f42157521d3
sibrikhanr98 <rmsifrikan6500@gmail.com>
2024-06-05 13:10:44 +0530

    created addEventListener function

---
+++ 04.Chrome Extension/index.js
@@ -0,0 +1,10 @@
+
+// function saveIn(){
+//     console.log("Button clicked from onlick attribute");
+// }
+
+let savebtn = document.getElementById('input-btn')
+
+savebtn.addEventListener("click",function(){
+    console.log("Button clicked from the addEventListener")
+})
