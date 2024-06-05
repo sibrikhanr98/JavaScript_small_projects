6047885ef780f81e6e8fdb74c2b01398dc04f087
sibrikhanr98 <rmsifrikan6500@gmail.com>
2024-06-05 20:30:22 +0530

    renderLead function created

--- 04.Chrome Extension/index.js
+++ 04.Chrome Extension/index.js
@@ -3,16 +3,40 @@
 //     console.log("Button clicked from onlick attribute");
 // }
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputbtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 
 inputbtn.addEventListener("click",function(){
     myLeads.push(inputEl.value);
-    console.log(myLeads) 
+    //console.log(myLeads)
+    rednerLeads()
 })
 
-for (let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+
+function rednerLeads(){
+    // one way to do
+    // for (let i = 0; i < myLeads.length; i++){
+    //     // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"\
+
+
+    //     // another way to render
+
+    //     // Create element
+    //     // set text content
+    //     // append to ul
+    //     const li = document.createElement('li')
+    //     li.textContent = myLeads[i]
+    //     ulEl.append(li)
+    // } 
+
+    // another way fast way
+    let list_item = "" 
+    for (let i = 0; i < myLeads.length; i++){
+        list_item += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = list_item
+
 }
+
