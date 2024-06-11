93b22dc288e4037444d0dfad9ec3a96ee950ecbe
sibrikhanr98 <rmsifrikan6500@gmail.com>
2024-06-11 13:33:33 +0530

    Chrome API has been added for navigate the current page in the browser

--- 04.Chrome Extension/index.js
+++ 04.Chrome Extension/index.js
@@ -33,15 +33,15 @@ if(leadsFromLocalStorage){
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
 
 tabBtn.addEventListener("click",function(){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    render(myLeads)
-    console.log(localStorage.getItem("myLeads"))
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { 
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+
 })
 
 
