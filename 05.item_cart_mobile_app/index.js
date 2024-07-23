ca7e84ac258394ffbc58efa66f553987c40e3445
sibrikhanr98 <rmsifrikan6500@gmail.com>
2024-07-23 14:08:51 +0530

    CSS updated for the app

---
+++ 05.item_cart_mobile_app/index.js
@@ -0,0 +1,8 @@
+const inputField = document.getElementById('input-field');
+const addButton = document.getElementById('add-button');
+
+addButton.addEventListener('click',function(){
+    let inputFieldValue = inputField.value;
+
+    console.log(inputFieldValue);
+})
